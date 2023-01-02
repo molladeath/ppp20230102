5f75c16662afb4e70db3f6bdd999e560d4f8b921
molladeath <molladeath@gmail.com>
2023-01-02 20:37:34 +0900

    죽겠다

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   const [open , setToggle] = useState(true);
   return (
-    <div className='body openpopup' >
+    <div className={ open ?'body openpopup' : 'body'}>
       <div id = 'pop'>
-        <button>닫기</button>
+        <button onClick={()=>{ setToggle(!open) }}>닫기</button>
         <p>화면 정가운데</p>
       </div>
     </div>
